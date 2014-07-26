843b27a575797b1b63886684dee59ae8d52af1f0
spiessbuerger <tobias47n9e@gmail.com>
2014-07-27 00:14:42 +0200

    removing badly named file

--- examples/empty-window.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-from gi.repository import Gtk
-
-my_first_window = Gtk.Window()
-
-my_first_window.connect("delete-event", Gtk.main_quit)
-my_first_window.show_all()
-Gtk.main()
